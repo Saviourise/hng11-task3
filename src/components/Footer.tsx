91d885ff07e1bc9d5d7ae675754cb7d4f809d74c
Saviour Ise <saviourise1@gmail.com>
2024-07-07 18:47:30 +0100

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import "../styles/Footer.css";
 import logo_white from "../assets/images/logo_white.png";
+import { SlSocialFacebook } from "react-icons/sl";
+import { HiOutlineMail } from "react-icons/hi";
+import { FaInstagram } from "react-icons/fa6";
+import { FaSnapchat } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <img src={logo_white} alt="logo" />
 
-      <div className="section1">
-        <h1>Company</h1>
-        <p>About</p>
-        <p>Our Stores</p>
-        <p>Bulk orders</p>
-        <p>Product testing</p>
-        <p>Terms and conditions</p>
-        <p>Affliate marketing</p>
+      <div>
+        <div className="section1">
+          <p>About Us</p>
+          <p>Blog</p>
+          <p>FAQs</p>
+          <p>Order Tracking</p>
+          <p>Contact</p>
+          <p>©Legacy jnr</p>
+        </div>
+
+        <div className="section2">
+          <FaSnapchat color="#fff" size={20} />
+          <FaInstagram color="#fff" size={20} />
+          <HiOutlineMail color="#fff" size={20} />
+          <SlSocialFacebook color="#fff" size={20} />
+        </div>
       </div>
     </div>
   );
